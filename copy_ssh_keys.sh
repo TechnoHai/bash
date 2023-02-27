6cefb6eb287f5d832b5ea100291ca170d62de0cc
sarusi79 <78098532+sarusi79@users.noreply.github.com>
2023-02-27 20:43:21 +0200

    Create copy_ssh_keys.sh

---
+++ copy_ssh_keys.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#get a list of hosts/ip from the hosts.txt file
+ips=$(cat hosts.txt)
+for ip in `ips`
+do
+    sshpass -p "password" ssh-copy-id -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa.pub user@$ip
+done
