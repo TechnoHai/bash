65f97c9e155b626c4bb60fb0f24fd1b54578b403
sarusi79 <78098532+sarusi79@users.noreply.github.com>
2023-03-04 22:06:48 +0200

    added rocky os and added a few comments

--- update_linux.sh
+++ update_linux.sh
@@ -3,25 +3,39 @@
 os=$(grep -w ID  /etc/os-release)
 
 #check the ID  field and using the right package managment to run update . 
+#check if the os is cetnos
 if [ $os == "ID="centos"" ]
 then
     echo "The OS is Centos "
     sleep 3
     echo "Updating And Upgrading The os"
     sudo yum update -y && sudo yum upgrade -y
-	
+
+#check if the os is fedora	
 elif [ $os == "ID=fedora" ]
 then
     echo "The OS is Fedora "
     sleep 3
     echo "Updating And Upgrading The os"
     sudo yum update -y && sudo yum upgrade -y
-	
+
+#check if the os is ubuntu	
 elif [[ $os == "ID=ubuntu" ]] 
 then
      echo "The OS is Ubunto "
      sleep 3
      echo "Updating And Upgrading The os "
      sudo apt update -y
-     sudo apt upgrade -y             
+     sudo apt upgrade -y  
+
+#check if the os is rocky
+elif [[ $os == "ID="rocky ]] 
+then
+     echo "The OS is rocky "
+     sleep 3
+     echo "Updating And Upgrading The os "
+     sudo apt update -y
+     sudo apt upgrade -y                            
 fi           
+
+
