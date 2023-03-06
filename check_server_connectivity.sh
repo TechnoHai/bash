1cbb28ee210b0350e9ef4aa964ef05bc70f355b1
sarusi79 <78098532+sarusi79@users.noreply.github.com>
2023-03-06 10:09:18 +0200

    created new script to check server connectivity

---
+++ check_server_connectivity.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+servers=$(cat hosts.txt)
+
+for server in $servers
+do
+	ping -c 2 $server
+
+	if [ $? -eq 0 ]
+	then
+		echo "###########################################"
+		echo "The server is alive!"
+		echo "###########################################"
+	else
+		echo "###########################################"
+		echo "The server is Dead check connectivity!"
+		echo "###########################################"
+	fi
+done
