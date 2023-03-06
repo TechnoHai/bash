66a3d9fca4a68f0ac0b8e0483d7d796a46163eee
sarusi79 <78098532+sarusi79@users.noreply.github.com>
2023-03-06 18:30:54 +0200

    added log files to the update system script

--- update_linux.sh
+++ update_linux.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #cheking ths os type by the ID field in os-release file
 os=$(grep -w ID  /etc/os-release)
+#log file for the updating procces
+log_file=/var/log/update_linux_log
+error_log=/var/log/update_linux_error
 
 #check the ID  field and using the right package managment to run update . 
 #check if the os is cetnos
@@ -9,7 +12,14 @@ then
     echo "The OS is Centos "
     sleep 3
     echo "Updating And Upgrading The os"
-    sudo yum update -y && sudo yum upgrade -y
+    sudo yum update -y && sudo yum upgrade -y 
+    #check if the update went succsfully or not and log it.
+    if [ $? -ne 0 ]
+    then
+        echo "An error accured please check the $error_log file"
+    else 
+        echo "The update procces finish succsefully, you can check $log_file for detalis"
+    fi
 
 #check if the os is fedora	
 elif [ $os == "ID=fedora" ]
@@ -18,6 +28,13 @@ then
     sleep 3
     echo "Updating And Upgrading The os"
     sudo yum update -y && sudo yum upgrade -y
+    #check if the update went succsfully or not and log it.
+    if [ $? -ne 0 ]
+    then
+        echo "An error accured please check the $error_log file"
+    else 
+        echo "The update procces finish succsefully, you can check $log_file for detalis"
+    fi
 
 #check if the os is ubuntu	
 elif [[ $os == "ID=ubuntu" ]] 
@@ -26,7 +43,14 @@ then
      sleep 3
      echo "Updating And Upgrading The os "
      sudo apt update -y
-     sudo apt upgrade -y  
+     sudo apt upgrade -y
+    #check if the update went succsfully or not and log it.
+    if [ $? -ne 0 ]
+    then
+        echo "An error accured please check the $error_log file"
+    else 
+        echo "The update procces finish succsefully, you can check $log_file for detalis"
+    fi  
 
 #check if the os is rocky
 elif [[ $os == "ID="rocky ]] 
@@ -35,7 +59,14 @@ then
      sleep 3
      echo "Updating And Upgrading The os "
      sudo apt update -y
-     sudo apt upgrade -y                            
+     sudo apt upgrade -y 
+    #check if the update went succsfully or not and log it.
+    if [ $? -ne 0 ]
+    then
+        echo "An error accured please check the $error_log file"
+    else 
+        echo "The update procces finish succsefully, you can check $log_file for detalis"
+    fi                           
 fi           
 
 
