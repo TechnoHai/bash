4dfda0a4b51542ada08c8db59b66db5285cb41a3
Hai Sarusi <hai.sarusi@gmail.com>
2023-03-06 10:30:10 +0200

    added the host.txt file and modified the server connectivity script

--- check_server_connectivity.sh
+++ check_server_connectivity.sh
@@ -3,16 +3,19 @@ servers=$(cat hosts.txt)
 
 for server in $servers
 do
-	ping -c 2 $server
+	ping -c 2 $server > /dev/null
 
 	if [ $? -eq 0 ]
 	then
+                echo
 		echo "###########################################"
-		echo "The server is alive!"
+		echo "The server $server is alive!"
 		echo "###########################################"
+                echo 
 	else
-		echo "###########################################"
-		echo "The server is Dead check connectivity!"
-		echo "###########################################"
+		echo "########################################################"
+		echo "The server $server is Dead check connectivity!"
+		echo "########################################################"
+                echo
 	fi
 done
