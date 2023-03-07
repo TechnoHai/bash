7f29fbcc1c4bbb9ab6405a2686674dbc1c48ada5
sarusi79 <78098532+sarusi79@users.noreply.github.com>
2023-03-07 20:25:52 +0200

    corrected some grammar on the script

--- update_linux.sh
+++ update_linux.sh
@@ -2,21 +2,21 @@
 #cheking ths os type by the ID field in os-release file
 os=$(grep -w ID  /etc/os-release)
 
-#log files for the updating procces
+#log files for the updating process
 log_file=/var/log/update_linux_log
 error_log=/var/log/update_linux_error
 
-#a function that check the rxit code after the update is runing and tell the user where to look for logs
+#a function that check the exit code after the update is running and tell the user where to look for logs
 check_exit_code() {
     if [ $? -ne 0 ]
     then
-        echo "An error accured please check the $error_log file"
+        echo "An error accrued please check the $error_log file"
     else 
-        echo "The update procces finish succsefully, you can check $log_file for detalis"
+        echo "The update process finish successfully, you can check $log_file for details"
     fi
 }
 
-#check the ID  field and using the right package managment to run update . 
+#check the ID  field and using the right package management to run update .
 #check if the os is cetnos
 if [ $os == "ID="centos"" ]
 then
@@ -24,7 +24,7 @@ then
     sleep 3
     echo "Updating And Upgrading The os"
     sudo yum update -y && sudo yum upgrade -y 1>$log_file 2>$error_log
-    #check if the update went succsfully or not and log it.
+    #check if the update went successfully or not and log it.
     check_exit_code
 
 #check if the os is fedora	
@@ -34,18 +34,18 @@ then
     sleep 3
     echo "Updating And Upgrading The os"
     sudo yum update -y && sudo yum upgrade -y 1>$log_file 2>$error_log
-    #check if the update went succsfully or not and log it.
+    #check if the update went successfully or not and log it.
     check_exit_code
 
 #check if the os is ubuntu	
 elif [[ $os == "ID=ubuntu" ]] 
 then
-     echo "The OS is Ubunto "
+     echo "The OS is Ubuntu "
      sleep 3
      echo "Updating And Upgrading The os "
      sudo apt update -y && sudo apt upgrade -y 1>$log_file 2>$error_log
      
-    #check if the update went succsfully or not and log it.
+    #check if the update went successfully or not and log it.
     check_exit_code
 
 #check if the os is rocky
@@ -56,7 +56,7 @@ then
      echo "Updating And Upgrading The os "
      sudo apt update -y && sudo apt upgrade -y 1>$log_file 2>$error_log
       
-    #check if the update went succsfully or not and log it.
+    #check if the update went successfully or not and log it.
    check_exit_code
 fi           
 
