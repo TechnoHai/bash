1caecec5faff67db535eae12fbd9a2a857daf0a6
Hai Sarusi <hai.sarusi@gmail.com>
2023-03-07 18:35:16 +0200

    added a function check_exit_code() to the update linux script

---
+++ install_exa.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#install exa ls alternativ
+
+sudo apt update
+sudo apt install -y unzip
+EXA_VERSION=$(curl -s "https://api.github.com/repos/ogham/exa/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
+curl -Lo exa.zip "https://github.com/ogham/exa/releases/latest/download/exa-linux-x86_64-v${EXA_VERSION}.zip"
+sudo unzip -q exa.zip bin/exa -d /usr/local
--- update_linux.sh
+++ update_linux.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#this is a script to updatge any linux distro
+
 #cheking ths os type by the ID field in os-release file
 os=$(grep -w ID  /etc/os-release)
 
@@ -6,7 +8,16 @@ os=$(grep -w ID  /etc/os-release)
 log_file=/var/log/update_linux_log
 error_log=/var/log/update_linux_error
 
-#check the ID  field and using the right package managment to run update . 
+check_exit_code() {
+ if [ $? -ne 0 ]
+    then
+        echo "An error accured please check the $error_log file"
+    else
+        echo "The update procces finish succsefully, you can check $log_file for detalis"
+    fi
+}
+
+#check the ID  field and using the right package managment to run update .
 #check if the os is cetnos
 if [ $os == "ID="centos"" ]
 then
@@ -14,60 +25,42 @@ then
     sleep 3
     echo "Updating And Upgrading The os"
     sudo yum update -y && sudo yum upgrade -y 1>$log_file 2>$error_log
-    #check if the update went succsfully or not and log it.
-    if [ $? -ne 0 ]
-    then
-        echo "An error accured please check the $error_log file"
-    else 
-        echo "The update procces finish succsefully, you can check $log_file for detalis"
-    fi
 
-#check if the os is fedora	
+    #check if the update went succsfully or not and log it.
+    check_exit_code()
+
+#check if the os is fedora
 elif [ $os == "ID=fedora" ]
 then
     echo "The OS is Fedora "
     sleep 3
     echo "Updating And Upgrading The os"
     sudo yum update -y && sudo yum upgrade -y 1>$log_file 2>$error_log
-    #check if the update went succsfully or not and log it.
-    if [ $? -ne 0 ]
-    then
-        echo "An error accured please check the $error_log file"
-    else 
-        echo "The update procces finish succsefully, you can check $log_file for detalis"
-    fi
 
-#check if the os is ubuntu	
-elif [[ $os == "ID=ubuntu" ]] 
+    #check if the update went succsfully or not and log it.
+    check_exit_code()
+
+#check if the os is ubuntu
+elif [[ $os == "ID=ubuntu" ]]
 then
      echo "The OS is Ubunto "
      sleep 3
      echo "Updating And Upgrading The os "
      sudo apt update -y && sudo apt upgrade -y 1>$log_file 2>$error_log
-     
+
     #check if the update went succsfully or not and log it.
-    if [ $? -ne 0 ]
-    then
-        echo "An error accured please check the $error_log file"
-    else 
-        echo "The update procces finish succsefully, you can check $log_file for detalis"
-    fi  
+    check_exit_code()
 
 #check if the os is rocky
-elif [[ $os == "ID="rocky ]] 
+elif [[ $os == "ID="rocky ]]
 then
      echo "The OS is rocky "
      sleep 3
      echo "Updating And Upgrading The os "
      sudo apt update -y && sudo apt upgrade -y 1>$log_file 2>$error_log
-      
+
     #check if the update went succsfully or not and log it.
-    if [ $? -ne 0 ]
-    then
-        echo "An error accured please check the $error_log file"
-    else 
-        echo "The update procces finish succsefully, you can check $log_file for detalis"
-    fi                           
-fi           
+    check_exit_code()
+fi
 
 
