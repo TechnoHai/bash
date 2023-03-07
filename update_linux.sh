8da4ecde7ed7cd0cb010789eafc4ce3ab3304db3
sarusi79 <78098532+sarusi79@users.noreply.github.com>
2023-03-07 18:59:12 +0200

    fixed the script ... when you call a function you dont use ()

--- update_linux.sh
+++ update_linux.sh
@@ -6,6 +6,15 @@ os=$(grep -w ID  /etc/os-release)
 log_file=/var/log/update_linux_log
 error_log=/var/log/update_linux_error
 
+check_exit_code() {
+    if [ $? -ne 0 ]
+    then
+        echo "An error accured please check the $error_log file"
+    else 
+        echo "The update procces finish succsefully, you can check $log_file for detalis"
+    fi
+}
+
 #check the ID  field and using the right package managment to run update . 
 #check if the os is cetnos
 if [ $os == "ID="centos"" ]
@@ -15,12 +24,7 @@ then
     echo "Updating And Upgrading The os"
     sudo yum update -y && sudo yum upgrade -y 1>$log_file 2>$error_log
     #check if the update went succsfully or not and log it.
-    if [ $? -ne 0 ]
-    then
-        echo "An error accured please check the $error_log file"
-    else 
-        echo "The update procces finish succsefully, you can check $log_file for detalis"
-    fi
+    check_exit_code
 
 #check if the os is fedora	
 elif [ $os == "ID=fedora" ]
@@ -30,12 +34,7 @@ then
     echo "Updating And Upgrading The os"
     sudo yum update -y && sudo yum upgrade -y 1>$log_file 2>$error_log
     #check if the update went succsfully or not and log it.
-    if [ $? -ne 0 ]
-    then
-        echo "An error accured please check the $error_log file"
-    else 
-        echo "The update procces finish succsefully, you can check $log_file for detalis"
-    fi
+    check_exit_code
 
 #check if the os is ubuntu	
 elif [[ $os == "ID=ubuntu" ]] 
@@ -46,12 +45,7 @@ then
      sudo apt update -y && sudo apt upgrade -y 1>$log_file 2>$error_log
      
     #check if the update went succsfully or not and log it.
-    if [ $? -ne 0 ]
-    then
-        echo "An error accured please check the $error_log file"
-    else 
-        echo "The update procces finish succsefully, you can check $log_file for detalis"
-    fi  
+    check_exit_code
 
 #check if the os is rocky
 elif [[ $os == "ID="rocky ]] 
@@ -62,12 +56,7 @@ then
      sudo apt update -y && sudo apt upgrade -y 1>$log_file 2>$error_log
       
     #check if the update went succsfully or not and log it.
-    if [ $? -ne 0 ]
-    then
-        echo "An error accured please check the $error_log file"
-    else 
-        echo "The update procces finish succsefully, you can check $log_file for detalis"
-    fi                           
+   check_exit_code
 fi           
 
 
