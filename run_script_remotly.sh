7276c17bc4175c53b32d35cb5ec643cb25f81fd7
Hai Sarusi <78098532+TechnoHai@users.noreply.github.com>
2023-03-12 21:33:09 +0200

    Update run_script_remotly.sh

--- run_script_remotly.sh
+++ run_script_remotly.sh
@@ -5,5 +5,6 @@ hosts=$(cat hosts.txt)
 for host in $hosts
 do
 # "bash -s" < run the script from local machine on remote host
-  ssh vagrant@$host "bash -s" < /opt/scripts/18_update_linux.sh
+  chmod +x update_linux.sh
+  ssh vagrant@$host "bash -s" < update_linux.sh
 done
