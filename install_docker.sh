eef070e7b8d6ecdb0c2f57214e0a4531a320ecdc
hai <hai.sarusi@gmail.com>
2024-03-18 13:01:06 +0200

    updated dokcer script

--- install_docker.sh
+++ install_docker.sh
@@ -2,15 +2,20 @@
 #Install dependencies
 sudo apt install apt-transport-https curl gnupg-agent ca-certificates software-properties-common -y
 #add gpe key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-#add docker repo
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-#install docker
-sudo apt install docker-ce docker-ce-cli container.io -y
-#user and permissions
-sudo usermod -aG docker $USER
-newgrp docker
-#enable the service
-sudo systemctl enable docker
-#test docker installation
-docker run hello-world
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo docker run hello-world
